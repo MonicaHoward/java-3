3e85de9e2ef6127fa3655a53dba0b004f1691f7b
Monica Howard <mo@Monicas-MacBook-Air.local>
2020-06-29 16:17:05 -0500

    completed Reasoning class

--- src/concurrency/Main.java
+++ src/concurrency/Main.java
@@ -3,6 +3,7 @@ package concurrency;
 public class Main {
    
     public static void main(String[] args) {
-         //run all of your threads from this main class.
+        Reasoning rThread = new Reasoning();
+        rThread.start();
     }
 }
--- src/concurrency/Reasoning.java
+++ src/concurrency/Reasoning.java
@@ -1,10 +1,14 @@
 package concurrency;
 
-class Reasoning {
-    //set up this class so it can become a valid thread. 
+class Reasoning extends Thread {
+
+    public void run(){
+    distinguish();
+    }
+
     void distinguish() {
-        //print to the console the difference between a thread and a process
-        //print out you think will happen if you invoke the run() method of a thread as opposed to the start() method of a thread.
+        System.out.println("The difference between a thread and a process is that a process is a program that is in the act of being executed. Before it is executed it is a program and once it has finished running it is a program. A process may contain a thread. Processes don't share memory space and are larger and more involved than threads. A thread is part of a process or program.");
+        System.out.println("If you invoke the run() method of a Thread class you will get an exception. The method that you should run is the .start() method");
     }
 }
 
